12b377f34f5f795df14930105ad70e97ac2e8635
Antonio D'aria <antoniodaria@msn.com>
2023-12-13 17:24:08 +0100

    refactored response

--- api/github_api/handler/get_fav_language.go
+++ api/github_api/handler/get_fav_language.go
@@ -6,12 +6,22 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type GetFavLanguageResponse struct {
+	UserName         string `json:"user_name"`
+	MostUsedLanguage string `json:"most_used_language"`
+}
+
 func (h *Handler) GetFavLanguage(c *fiber.Ctx) error {
-	language, err := h.GithubClient.GetFavLanguage(c.Context(), c.Params("userName"))
+	userName := c.Params("userName")
+
+	language, err := h.GithubClient.GetFavLanguage(c.Context(), userName)
 	if err != nil {
 		fmt.Printf("Failed to get favourite language %v", err)
 		return c.Status(fiber.StatusInternalServerError).JSON(err.Error())
 	}
 
-	return c.JSON(language)
+	return c.JSON(GetFavLanguageResponse{
+		MostUsedLanguage: language,
+		UserName:         userName,
+	})
 }
